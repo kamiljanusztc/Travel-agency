3df7969b4dacd8ecfd1f732f853bdfecf2f21f5f
Kamil Janusz <kamiljanusztc@gmail.com>
2021-02-14 19:38:57 +0100

    Active filters

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import TripListOptions from './TripListOptions';
+import { getAllTags } from '../../../redux/tagsRedux';
+import { getAllFilters, changeSearchPhrase, changeDuration, addTag, removeTag } from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // TODO - add more dispatchers for other filters - wykorzystanie kreatorów akcji do powiązania dispatcherów z propsami komponentu
+  changeDuration: duration => dispatch(changeDuration(duration)),
+  addTag: tag => dispatch(addTag(tag)),
+  removeTag: tag => dispatch(removeTag(tag)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
