078081f6d2ff46e229cef7d9735cee560170a500
Kamil Janusz <kamiljanusztc@gmail.com>
2021-02-17 21:15:35 +0100

    Add trip options

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Row, Col } from 'react-flexbox-grid';
+import OrderSummary from '../OrderSummary/OrderSummary';
+import PropTypes from 'prop-types';
+
+const OrderForm = ({ tripCost, options }) => (
+  <Row>
+    <Col xs={12}>
+      <OrderSummary tripCost={tripCost} options={options} />
+    </Col>
+  </Row>
+);
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderForm;
