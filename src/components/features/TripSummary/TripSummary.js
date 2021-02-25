778e41598e66360f581f65d9c59cebbd4d176767
Kamil Janusz <kamiljanusztc@gmail.com>
2021-02-25 20:10:20 +0100

    Add test to TripSummary

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './TripSummary.scss';
-import {Col} from 'react-flexbox-grid';
+import { Col } from 'react-flexbox-grid';
 
-const TripSummary = ({id, image, name, cost, days, tags}) => (
+const TripSummary = ({ id, image, name, cost, days, tags }) => (
   <Col xs={12} sm={6} lg={4} className={styles.column}>
     <Link to={`/trip/${id}`} className={styles.link}>
       <article className={styles.component}>
         <img src={image} alt={name} />
         <h3 className={styles.title}>{name}</h3>
         <div className={styles.details}>
-          <span>{days} days</span>
-          <span>from {cost}</span>
+          <span>{days}
+          days</span>
+          <span>from
+            {cost}</span>
         </div>
         <div className={styles.tags}>
           {tags.map(tag => (
@@ -25,8 +27,8 @@ const TripSummary = ({id, image, name, cost, days, tags}) => (
 );
 
 TripSummary.propTypes = {
-  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  image: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  image: PropTypes.string.isRequired,
   name: PropTypes.string,
   intro: PropTypes.string,
   cost: PropTypes.string,
---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+  it('should render correct link', () => {
+    const expectedId = 'abc';
+    const expectedSrc = 'image.jpg';
+    const component = shallow(<TripSummary id={expectedId} tags={['']} image={expectedSrc}
+    />);
+
+    const renderedLink = component.find('.link').prop('to');
+    expect(renderedLink).toEqual('/trip/' + expectedId);
+  });
+
+  it('should render correct src and alt', () => {
+    const expectedId = 'abc';
+    const expectedSrc = 'image.jpg';
+    const expectedAlt = 'trip';
+    const component = shallow(<TripSummary id={expectedId} image={expectedSrc} name={expectedAlt} tags={['']} />);
+
+    const renderedSrc = component.find('img').prop('src');
+    const renderedAlt = component.find('img').prop('alt');
+
+    expect(renderedSrc).toEqual(expectedSrc);
+    expect(renderedAlt).toEqual(expectedAlt);
+  });
+
+  it('should render name, cost and days correctly', () => {
+    const expectedName = 'trip';
+    const expectedCost = '100';
+    const expectedDays = 2;
+    const expectedId = 'abc';
+    const expectedSrc = 'image.jpg';
+    const component = shallow(<TripSummary name={expectedName} cost={expectedCost} days={expectedDays} id={expectedId} tags={['']} image={expectedSrc} />);
+
+    const renderedName = component.find('.title').text();
+    const renderedCost = component.find('span').at(1).text();
+    const renderedDays = component.find('span').at(0).text();
+
+    expect(renderedName).toEqual(expectedName);
+    expect(renderedCost).toEqual('from' + expectedCost);
+    expect(renderedDays).toEqual(expectedDays + 'days');
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<TripSummary />)).toThrow();
+  });
+
+  it('should render tags', () => {
+    const expectedSrc = 'image.jpg';
+
+    const component = shallow(<TripSummary tags={['firstTag', 'secondTag', 'thirdTag']} image={expectedSrc} />);
+
+    expect(component.find('.tag').at(0).text()).toEqual('firstTag');
+    expect(component.find('.tag').at(1).text()).toEqual('secondTag');
+    expect(component.find('.tag').at(2).text()).toEqual('thirdTag');
+  });
+
+  it('should not render tags when array is empty or tags is false', () => {
+    const expectedSrc = 'image.jpg';
+
+    const component = shallow(<TripSummary tags={[]} image={expectedSrc} />);
+
+    expect(component.find('tags')).toMatchObject({});
+  });
+
+});
